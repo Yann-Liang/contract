d7e04850ef325a8a79ec04e8e302994bb231f4fd
liangyanxiang <liangyanxiang@juzix.io>
2019-01-23 13:58:57 +0800

    update

---
+++ candidateConstract.js
@@ -0,0 +1,24 @@
+const { getCandidateList, candidateDetails, candidateDeposit, verifiersList, candidateWithdrawInfos, setCandidateExtra, candidateApplyWithdraw, candidateWithdraw} = require ('./lib/validation.1');
+
+
+const result = getCandidateList();
+
+candidateDeposit()
+
+candidateDetails()
+
+verifiersList()
+
+candidateWithdrawInfos()
+
+setTimeout(() => {
+    setCandidateExtra()
+
+    candidateApplyWithdraw()
+
+
+}, 3500)
+
+setTimeout(() => {
+    candidateWithdraw();
+}, 5000)
