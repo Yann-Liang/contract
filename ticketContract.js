657c11547909e19232fcd2ec3893c5a2794f3750
liangyanxiang <liangyanxiang@juzix.io>
2019-01-23 17:04:31 +0800

    添加票池

---
+++ ticketContract.js
@@ -0,0 +1,17 @@
+const { voteTicket,
+    getTicketDetail,
+    getBatchTicketDetail,
+    getCandidateTicketIds,
+    getBatchCandidateTicketIds,
+    getCandidateEpoch,
+    getPoolRemainder,
+    getTicketPrice } = require('./lib/ticketContract');
+
+getTicketPrice()
+voteTicket()
+getTicketDetail()
+getBatchTicketDetail()
+getCandidateTicketIds()
+getBatchCandidateTicketIds()
+getCandidateEpoch()
+getPoolRemainder()
